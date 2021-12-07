7d7034d99d2bfa3ba398a38c053afc746bd3ce3c
Matthew Darwin <matt.darwin@slalom.com>
2021-12-07 15:49:17 +0000

    add vagrant box to build catalina with xcode, ansible and brew

---
+++ macOS_Catalina/scripts/ansible_install.sh
@@ -0,0 +1,11 @@
+sudo -H python -m ensurepip
+sudo python -m pip -q install --upgrade pip
+
+sudo su - vagrant -c 'echo "export PATH=$PATH:/Users/vagrant/Library/Python/2.7/bin" > ~/.zprofile'
+
+sudo su - vagrant -c "pip install --user --no-warn-script-location wheel tornado nose"
+sudo su - vagrant -c "pip install --user ansible==4"
+
+export UPDATENAME=$(softwareupdate -l | grep Title | awk -F'([:,])' '{print $2}')
+sudo softwareupdate -ia "macOS Catalina 10.15.7 Update"
+
